37ddda4c2beea392cd63c2f3379accf7562be092
Lama Saeeda <saeedla1@fel.cvut.cz>
2018-09-05 20:47:08 +0200

    [add] Morphodita rest controller

---
+++ src/main/java/cz/cvut/kbss/textanalysis/rest/MorphoDitaController.java
@@ -0,0 +1,24 @@
+package cz.cvut.kbss.textanalysis.rest;
+
+import cz.cvut.kbss.textanalysis.model.MorphoDitaResult;
+import cz.cvut.kbss.textanalysis.service.MorphoDitaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MorphoDitaController {
+
+    @Autowired
+    private MorphoDitaService morphoDitaService;
+
+    @RequestMapping("/hello")
+    public String sayHi() {
+        return "Hi";
+    }
+
+    @RequestMapping("/morphodita-result")
+    public MorphoDitaResult getMorphoditaResult() {
+        return morphoDitaService.morphoDitaGetResult();
+    }
+}

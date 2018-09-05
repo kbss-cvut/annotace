866ed4a2238a114b599ce63a3a8a22930d2041b0
Lama Saeeda <saeedla1@fel.cvut.cz>
2018-09-05 20:49:24 +0200

    [add] Morphodita business service

---
+++ src/main/java/cz/cvut/kbss/textanalysis/service/MorphoDitaService.java
@@ -0,0 +1,24 @@
+package cz.cvut.kbss.textanalysis.service;
+
+import cz.cvut.kbss.textanalysis.model.MorphoDitaResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class MorphoDitaService {
+
+    private final RestTemplate restTemplate;
+
+        private MorphoDitaResult morphoDitaResult;
+
+    public MorphoDitaService(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public MorphoDitaResult morphoDitaGetResult() {
+        morphoDitaResult = this.restTemplate.getForObject("http://lindat.mff.cuni.cz/services/morphodita/api/analyze?data=Děti pojedou k babičce. Už se těší.", MorphoDitaResult.class);
+        return morphoDitaResult;
+    }
+}

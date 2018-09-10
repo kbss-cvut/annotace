297c0b8dfb3fab7f24c3f6afad61e3c0f394471c
Lama Saeeda <saeedla1@fel.cvut.cz>
2018-09-10 10:42:06 +0200

    [fix] read string from file input

--- src/main/java/cz/cvut/kbss/textanalysis/service/MorphoDitaService.java
+++ src/main/java/cz/cvut/kbss/textanalysis/service/MorphoDitaService.java
@@ -1,10 +1,13 @@
 package cz.cvut.kbss.textanalysis.service;
 
 import cz.cvut.kbss.textanalysis.model.MorphoDitaResult;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Service
 public class MorphoDitaService {
@@ -13,12 +16,25 @@ public class MorphoDitaService {
 
     private MorphoDitaResult morphoDitaResult;
 
-    public MorphoDitaService(RestTemplateBuilder restTemplateBuilder) {
+    String path = "C:/Projects/OPPPR/services/textanalysis/src/main/resources/sample.txt";
+    String s = "";
+
+
+    public MorphoDitaService(RestTemplateBuilder restTemplateBuilder) throws IOException {
         this.restTemplate = restTemplateBuilder.build();
     }
 
     public MorphoDitaResult getMorphoDitaResult() {
+        try
+        {
+            s = new String(Files.readAllBytes(Paths.get(path)));
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        System.out.println(s);
         morphoDitaResult = this.restTemplate.getForObject("http://lindat.mff.cuni.cz/services/morphodita/api/analyze?data=Děti pojedou k babičce. Už se těší.", MorphoDitaResult.class);
+//        morphoDitaResult = this.restTemplate.getForObject("http://lindat.mff.cuni.cz/services/morphodita/api/analyze?data=" + s, MorphoDitaResult.class);
         return morphoDitaResult;
     }
 }

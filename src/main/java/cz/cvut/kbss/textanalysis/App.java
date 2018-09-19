c9d751d3c2bb5a93b87e05b5e01c45d6f155b914
Petr Kremen <kremep1@fel.cvut.cz>
2018-09-20 01:02:01 +0200

    [UPD] Spring Boot Tomcatization

--- src/main/java/cz/cvut/kbss/textanalysis/App.java
+++ src/main/java/cz/cvut/kbss/textanalysis/App.java
@@ -2,9 +2,10 @@ package cz.cvut.kbss.textanalysis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer {
 
     public static void main(String[] args){
         SpringApplication.run(App.class);

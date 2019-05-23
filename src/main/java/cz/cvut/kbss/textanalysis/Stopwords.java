6832a115f1c64a1123bafd66cf57680127219eae
Lama Saeeda <saeedla1@fel.cvut.cz>
2019-05-23 15:39:17 +0200

    [fix] read stopwords from file to list

--- src/main/java/cz/cvut/kbss/textanalysis/Stopwords.java
+++ src/main/java/cz/cvut/kbss/textanalysis/Stopwords.java
@@ -1,24 +1,19 @@
 package cz.cvut.kbss.textanalysis;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
 
 public class Stopwords {
 
     public List<String> getStopwords(){
-        List<String> stopwordsList = new ArrayList<>();
         try {
-            Scanner scanner = new Scanner(new File(Stopwords.class.getResource("/stopwords-Czech.txt").getFile()));
-            while (scanner.hasNextLine()){
-                stopwordsList.add(scanner.next());
-            }
-            scanner.close();
-        } catch (FileNotFoundException e) {
+            return Files.readAllLines(new File(Stopwords.class.getClassLoader().getResource("stopwords-Czech.txt").getFile()).toPath());
+        } catch (IOException e) {
             e.printStackTrace();
+            return Collections.emptyList();
         }
-        return stopwordsList;
     }
 }

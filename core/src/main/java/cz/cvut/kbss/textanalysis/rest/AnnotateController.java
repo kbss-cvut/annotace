43d59a22a2e68e4809bb6bdf2ec2682c22d6de3a
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2026-06-11 14:36:10 +0200

    Switch to Spotless for license header generation, regenerate license headers.

--- core/src/main/java/cz/cvut/kbss/textanalysis/rest/AnnotateController.java
+++ core/src/main/java/cz/cvut/kbss/textanalysis/rest/AnnotateController.java
@@ -1,6 +1,6 @@
 /*
  * Annotace
- * Copyright (C) 2024 Czech Technical University in Prague
+ * Copyright (C) 2026 Czech Technical University in Prague
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,7 +13,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 package cz.cvut.kbss.textanalysis.rest;
 
